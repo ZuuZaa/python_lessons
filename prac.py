90a6258c79651bb1b99aac6cd1a5880c7c3944db
ZuuZaa <zumbaghirova@gmail.com>
2020-01-10 15:44:52 +0400

    commit

--- prac.py
+++ prac.py
@@ -75,7 +75,17 @@ name ="zumrud"
 #     print(i)
 
     
-list = [1,1,2,34,54,23]
+list = [1,1, 2,34,54,23]
+
+# #--- delete the last item of list
+# print(list.pop()) 
+# #--- delete item with index
+# del list[3]
+# print(list)
+# #--- delete Item with value
+# while 1 in list:
+#     list.remove(1)
+# print(list)
 
 # for i in list:
 #     print(i)
@@ -172,9 +182,176 @@ my_dict = {
 # print(my_dict)
 # print(d)
 
-d = {}
-d.update(my_dict)
-my_dict['name'] = "ali"
-d['age'] = 37
-print(my_dict)
-print(d)
+# d = {}
+# d.update(my_dict)
+# my_dict['name'] = "ali"
+# d['age'] = 37
+# print(my_dict)
+# print(d)
+
+
+# d = {
+#     'user1' : {'name':'zumrud', 'age':37},
+#     'user2': {'name':'ali', 'age':30},
+#     'user3': {'name':'mohammed', 'age':40}
+# }
+
+# for key, value in d.items():
+#     print(key, value)
+
+d = {
+    "movie1": 2003,
+    "movie2": 2005,
+    "movie3": 2007,
+    "movie4": 2003,
+    "movie5": 2001
+}
+
+# year = int(input('please enter year of movie  '))
+
+# for k, v in d.items():
+#     if v == year:
+#         print(k)
+#     else: 
+#         print('movie is not found')
+#         break
+
+# movie_list = [k for (k,v) in d.items() if v == year ]
+
+# print(movie_list)
+
+# for k in d:
+#     print(k)
+
+# for k in d.items():
+#     print(k)
+
+# for k, v in d.items():
+#     print(k, v)
+
+# my_d = [(k, v) for (k,v) in d.items() ]
+# print(my_d)
+
+# for i in d.values():
+#     print(i)
+
+
+# ---------- While -----------
+
+i=0
+
+# while i<=5:
+#     print(i)
+#     i= i+1
+
+# while i<=10:
+#     if i%2 ==0:
+#         print(i)
+#     i=i+1
+
+# str = "zumrud"
+# print("len str is:  ", len(str))
+# while i< len(str):
+#     print('i is:  ', i)
+#     print(str[i])
+#     i=i+1
+
+# while True:
+#     i = i+1
+#     if i % 2 == 0:
+#         print(f'skipping {i}')
+#     else:
+#         print(i)
+#     if i == 10:
+#         print("break")
+#         break
+# print("finish")
+    
+# active_users = ['u1', 'u2', 'u3']
+# users = []
+
+# while active_users:
+#     current_user = active_users.pop()
+#     print('current user is:  ', current_user)
+#     users.append(current_user)
+# print(users)
+
+# for i in users[::-1]:
+#     print(i.title())
+
+
+# while True:
+#     print('enter your name:')
+#     name = input(">> ")
+#     if name == "exit" or name == "e":
+#         break
+#     print("for exit press 'e' or enter 'exit'")
+ 
+   
+# res = {}
+
+# active = True
+# prompt= "> "
+
+# while active:
+#     print('enter your name')
+#     name = input(prompt)
+#     response = input("please enter your family_name:  ")
+#     res[name] = response
+#     print('do you want to repeat')
+#     repeat = input("y / n >>>  ")
+#     if repeat == "n":
+#         active = False
+
+# for k, v in res.items():
+#     print(k, "___", v)
+
+# words = ['hi', 'hello', 'sorry', 'by']
+
+# counter = 0
+
+# while counter < len(words):
+#     print(words[counter],'!')
+#     counter += 1
+
+
+# ---------- Calculator --------
+
+while True:
+    print('Welcome, this is a simple calculator.')
+    print("Enter + , - , * , / or ** for calculating.")
+    print("Press e for exit.")
+    users_input = input(">> ")
+
+    if users_input == "e":
+        break
+    elif users_input == "+":
+        print("Please enter two numbers for addition:")
+        a = float(input('first number >>  ')) 
+        b = float(input('second number >>  '))
+        c = a+b
+    elif users_input == "-":
+        print("Please enter two numbers for subtraction:")
+        a = float(input('first number >>  ')) 
+        b = float(input('second number >>  '))
+        c = a-b
+    elif users_input == "*":
+        print("Please enter two numbers for mutliplicate:")
+        a = float(input('first number >>  ')) 
+        b = float(input('second number >>  '))
+        c = a*b 
+    elif users_input == "/":
+        print("Please enter two numbers for divide:")
+        a = float(input('first number >>  ')) 
+        b = float(input('second number >>  '))
+        c = a/b
+    elif users_input == "**":
+        print("Please enter two numbers for power:")
+        a = float(input('first number >>  ')) 
+        b = float(input('second number >>  '))
+        c = a**b
+
+    print(f'The answer is >> {c}')
+
+print("Thanks for using our calculator :)")
+
